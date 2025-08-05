8834f043c33d9a04e1d5fd39e944b440f178d8b5
AvGeeky <sai.saip2005@gmail.com>
2025-08-05 19:05:04 +0530

    Initial commit

---
+++ src/main/java/com/events/paymentverifsystem/PaymentVerifSystemApplication.java
@@ -0,0 +1,13 @@
+package com.events.paymentverifsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaymentVerifSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaymentVerifSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/events/paymentverifsystem/PaymentVerifSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.events.paymentverifsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaymentVerifSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
